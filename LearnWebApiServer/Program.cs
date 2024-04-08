3b34a6f3e76b8fcb850da8c347353c2a35b00ec4
Home <Home@LAPTOP-2E6V6ACM>
2024-04-08 09:26:24 +0300

    Add project files.

---
+++ LearnWebApiServer/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LearnWebApiServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //הגדרות שנרצה להוסיף לפני בניית השרת
+            
+            var app = builder.Build();
+            //תוספות שנרצה להוסיף לשרת
+           
+
+
+
+            app.Run();
+        }
+    }
+}
